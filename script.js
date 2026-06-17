e5112eb8647dfa7d82860df04919907e7a6b8d7a
SpecialSpectator <sandveceextreme@gmail.com>
2026-06-18 01:05:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -743,7 +743,7 @@ function loadJS(FILE_URL) {
                 offset += 4;
                 leaderBoard = [];
                 for (i = 0; i < LBplayerNum; ++i) {
-					if(i == 15){
+					if(i == 10){
 						break;
 					}
                     var nodeId = msg.getUint32(offset, true);
